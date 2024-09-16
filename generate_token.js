7197f15150895f6e89c06f5ca4299e9d00b42548
WalterPaixaoCortes <wpcortes@gmail.com>
2024-09-16 01:35:22 -0300

    fix: refreshing data

--- generate_token.js
+++ generate_token.js
@@ -3,14 +3,14 @@ const readline = require('readline')
 const { google } = require('googleapis')
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json'
+const TOKEN_PATH = 'keys/token.json'
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
+fs.readFile('keys/credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err)
   // Authorize a client with credentials, then call the Google Sheets API.
   authorize(JSON.parse(content), listMajors)
